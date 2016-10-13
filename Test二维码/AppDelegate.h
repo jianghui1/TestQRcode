bf82ae2a2bf4acf14c1af9baad0086ee1be0699b
jiangzenghui005@163.com <jiangzenghui005@163.com>
2016-10-13 14:36:51 +0800

    上传测试二维码demo

---
+++ Test二维码/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test二维码
+//
+//  Created by ys on 16/3/4.
+//  Copyright © 2016年 jzh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Test二维码/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test二维码
+//
+//  Created by ys on 16/3/4.
+//  Copyright © 2016年 jzh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
